0698650d6b1f85721eb8dbaeda0e1be95cf83752
Nikita Savanchuk <savanchuk.nikita@gmail.com>
2017-09-16 22:06:19 +0300

    parser complite

--- parser.py
+++ parser.py
@@ -22,27 +22,6 @@ groupsHtml = BeautifulSoup(
     'html.parser'
 ).find('select', {'name': 'group'}).findAll('option')
 
-groups = []
-for group in groupsHtml:
-    groups.append(group.text)
-
-
-
-group_name = 'ПОКС-42'
-
-siteHtml = requests.post('http://m.rksi.ru',
-                         {'group': group_name.encode('cp1251'), 'stt': u'Показать!'.encode('cp1251')})
-
-data = siteHtml.text
-
-soup = BeautifulSoup(data, "html.parser")
-
-content = soup.find("div", {"class": "text"}).find('div')
-
-lessons = []
-lessonIndex = 0
-
-# Group.create(group_name)
 def parse_prefix(line, fmt):
     try:
         t = datetime.strptime(line, fmt)
@@ -54,52 +33,73 @@ def parse_prefix(line, fmt):
             raise
     return t
 
+for group_name in groupsHtml:
+    group_name = group_name.text
 
-for tag in content.children:
-    if tag.name == 'h3' or tag.name == 'form' or tag.name == 'br' or type(tag) is NavigableString:
-        continue
-
-    if tag.name == 'hr':
-        lessonIndex += 1
-        continue
-
-    if len(lessons) != lessonIndex + 1:
-        lessons.append([])
-
-    lessons[lessonIndex].append(tag)
-
-classedLessons = []
-
-for data in lessons:
-
-    dat = data.pop(0).text.split(', ')[0]
-
-    dt = parse_prefix(dat, '%d %b').replace(year=2017).date()
     with db_session:
-        Lesson.select(lambda l: l.date == dt).delete()
-    currentInfoCounter = 0
+        if len(Group.select(lambda x: x.name == group_name)) == 0:
+            Group.create(group_name)
 
-    for item in data:
-        time = ''
-        audience = ''
-        lecturer = ''
-        name = ''
-        for lessonInfo in item.children:
-            if lessonInfo.name == 'br':
-                continue
+    siteHtml = requests.post('http://m.rksi.ru',
+                             {'group': group_name.encode('cp1251'), 'stt': u'Показать!'.encode('cp1251')})
 
-            if type(lessonInfo) is NavigableString:
-                if currentInfoCounter == 0:
-                    time = lessonInfo
-                    currentInfoCounter += 1
-                elif currentInfoCounter == 1:
-                    lecturer, audience = lessonInfo.split(', ')
-                    currentInfoCounter = 0
-                    audience = audience.split('. ')[1]
-            else:
-                name = lessonInfo.text
-        Lesson.create(name, lecturer, audience, dt, time, group_name)
-        classedLesson = []
+    data = siteHtml.text
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    content = soup.find("div", {"class": "text"}).find('div')
+
+    lessons = []
+    lessonIndex = 0
+
+
+    for tag in content.children:
+        if tag.name == 'h3' or tag.name == 'form' or tag.name == 'br' or type(tag) is NavigableString:
+            continue
+
+        if tag.name == 'hr':
+            lessonIndex += 1
+            continue
+
+        if len(lessons) != lessonIndex + 1:
+            lessons.append([])
+
+        lessons[lessonIndex].append(tag)
+
+    classedLessons = []
+
+    for data in lessons:
+
+        dat = data.pop(0).text.split(', ')[0]
+
+        dt = parse_prefix(dat, '%d %b').replace(year=2017).date()
+        with db_session:
+            Lesson.select(lambda l: l.date == dt and l.group.name == group_name).delete()
+        currentInfoCounter = 0
+
+        for item in data:
+            time = ''
+            audience = ''
+            lecturer = ''
+            name = ''
+            for lessonInfo in item.children:
+                if lessonInfo.name == 'br':
+                    continue
+
+                if type(lessonInfo) is NavigableString:
+                    if currentInfoCounter == 0:
+                        time = lessonInfo
+                        currentInfoCounter += 1
+                    elif currentInfoCounter == 1:
+                        lecturer, audience = lessonInfo.split(', ')
+                        currentInfoCounter = 0
+                        audience = audience.split('. ')[1]
+                else:
+                    name = lessonInfo.text
+            Lesson.create(name, lecturer, audience, dt, time, group_name)
+            classedLesson = []
+
+
 with db_session:
     all_tables = Lesson.select()[:]
     print(all_tables)
